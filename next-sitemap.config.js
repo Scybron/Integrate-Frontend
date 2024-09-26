3a12f1dc52e29250b66e8679b31a22cd3a42cfab
alafzah123 <email.alafzah@gmail.com>
2024-09-26 19:28:30 +0530

    feat(seo-optimisation): seo optimisation

---
+++ next-sitemap.config.js
@@ -0,0 +1,5 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://www.integrateedu.co.in', // Replace this with your website URL
+    generateRobotsTxt: true, // (Optional) This generates a robots.txt file
+};
